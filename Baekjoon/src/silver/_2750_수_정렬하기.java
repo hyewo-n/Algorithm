5cf71da4d2232a0dc00993e1009628e8dfeb836c
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-26 21:19:06 +0900

    .

---
+++ Baekjoon/src/silver/_2750_수_정렬하기.java
@@ -0,0 +1,22 @@
+package silver;
+import java.util.*;
+
+public class _2750_수_정렬하기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		for(int x : arr) {
+			System.out.println(x);
+		}
+	}
+}
