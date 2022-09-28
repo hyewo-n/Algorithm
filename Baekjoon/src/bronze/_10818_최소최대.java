d0ffef9bb05bf5017e9eda2a9896778ecc205692
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-28 23:49:20 +0900

    .

---
+++ Baekjoon/src/bronze/_10818_최소최대.java
@@ -0,0 +1,22 @@
+package bronze;
+import java.util.*;
+
+public class _10818_최소최대 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] + " " + arr[n-1]);
+
+	}
+
+}
