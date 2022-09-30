9736e6f44d2bdcece936fec5297c015757fd0732
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-30 23:13:11 +0900

    .

---
+++ Baekjoon/src/bronze/_2884_알람시계.java
@@ -0,0 +1,28 @@
+package bronze;
+import java.util.*;
+
+public class _2884_알람시계 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(m<45) {
+			m = m + 60 - 45;
+			if(h==0)
+				h = 23;
+			else
+				h = h -1;
+		}
+		else {
+			m = m -45;
+		}
+		
+		System.out.println(h + " " + m);
+
+	}
+
+}
