50e5ea916e8a96cafabf961d67b6def4a19f27c7
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-04 03:22:15 +0900

    .

---
+++ Baekjoon/src/bronze/_8958_OX퀴즈.java
@@ -0,0 +1,29 @@
+package bronze;
+import java.util.*;
+
+public class _8958_OXÄûÁî {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			String str = sc.next();
+			int answer=0;
+			int sum=0;
+			for(int j=0; j<str.length(); j++) {
+				if(str.charAt(j)=='O') {
+					answer++;
+					sum += answer;
+				} else {
+					answer=0;
+				}
+			}
+			System.out.println(sum);
+		}
+		
+	}
+
+}
