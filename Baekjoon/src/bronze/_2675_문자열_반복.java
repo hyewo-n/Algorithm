c2b6746c580357571a31d5f315f214b71d70c735
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-03 23:15:05 +0900

    .

---
+++ Baekjoon/src/bronze/_2675_문자열_반복.java
@@ -0,0 +1,27 @@
+package bronze;
+import java.util.*;
+
+public class _2675_문자열_반복 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++) {
+			int r = sc.nextInt();
+			String s = sc.next();
+
+			String answer = "";
+			
+			for(int j=0; j<s.length(); j++) {
+				for(int k=0; k<r; k++) {
+					answer += s.charAt(j);
+				}
+			}
+			System.out.println(answer);
+		}
+
+	}
+
+}
