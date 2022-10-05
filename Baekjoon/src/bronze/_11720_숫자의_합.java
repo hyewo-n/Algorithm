1f21df3d3a5d6abc9874539d67efcecae417f211
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-06 03:27:12 +0900

    .

---
+++ Baekjoon/src/bronze/_11720_숫자의_합.java
@@ -0,0 +1,23 @@
+package bronze;
+import java.util.*;
+
+public class _11720_숫자의_합 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String str = sc.next();
+		
+		int sum=0;
+		int a=0;
+		for(int i=0; i<n; i++) {
+			a = str.charAt(i)-'0';
+			sum+=a;
+		}
+		System.out.println(sum);
+
+	}
+
+}
