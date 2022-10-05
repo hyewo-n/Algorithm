8e59a7ae8c0348bdd14d97ec037e002c05faf656
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-06 03:21:49 +0900

    .

---
+++ Baekjoon/src/bronze/_다시도전_2775_부녀회장이_될테야.java
@@ -0,0 +1,19 @@
+package bronze;
+import java.util.*;
+
+public class _다시도전_2775_부녀회장이_될테야 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+			
+		}
+
+	}
+
+}
