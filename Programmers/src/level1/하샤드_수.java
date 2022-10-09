c35a459fea6aeec2f0f29def9251724ca2c45902
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-09 22:12:01 +0900

    .

---
+++ Programmers/src/level1/하샤드_수.java
@@ -0,0 +1,32 @@
+package level1;
+
+import java.util.Scanner;
+
+public class ÇÏ»þµå_¼ö {
+
+	 public static boolean solution(int x) {
+	        boolean answer = true;
+	        
+	        int sum=0;
+	        String arr = String.valueOf(x);
+	        for(int i=0; i<arr.length(); i++) {
+	        	sum += arr.charAt(i)-'0';
+	        }
+	        
+	        if(x%sum!=0) {
+	        	answer = false;
+	        }
+	  
+	        return answer;
+	    }
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 11;
+		System.out.println(solution(n));
+		
+	}
+
+}
