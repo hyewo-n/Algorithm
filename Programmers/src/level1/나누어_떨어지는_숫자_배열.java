e5148483f2a37cdd72a317fc1f75e3fbb98599be
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-09 22:33:49 +0900

    .

---
+++ Programmers/src/level1/나누어_떨어지는_숫자_배열.java
@@ -0,0 +1,27 @@
+package level1;
+import java.util.*;
+
+public class 나누어_떨어지는_숫자_배열 {
+
+	public static int[] solution(int[] arr, int divisor) {
+		
+        int[] answer = new int[divisor];
+        
+        for(int i=0; i<answer.length; i++) {
+        	if(arr[i]%divisor==0) {
+        		
+        	}
+        }
+
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		int arr[] = {5, 9, 7, 10};
+		int n = 5;
+		System.out.println(solution(arr, n));
+		
+	}
+
+}
