5960fb7001b0cd298ae27b186f90baa5f5a81b03
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-11 23:27:50 +0900

    .

--- Programmers/src/level1/없는_숫자_더하기.java
+++ Programmers/src/level1/없는_숫자_더하기.java
@@ -2,9 +2,26 @@ package level1;
 
 public class 없는_숫자_더하기 {
 
+	public static int solution(int[] numbers) {
+        int answer = 0;
+        int sum=0;
+        
+        for(int i=1; i<10; i++) {
+        	for(int j=0; j<numbers.length; j++) {
+        		if(numbers[j] == i)
+        			sum += i;
+        	}
+        }
+        answer = 45-sum;
+        
+        return answer;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int arr[] = {5, 8, 4, 0, 6, 7, 9};
+		System.out.println(solution(arr));
+		
 	}
 
 }
