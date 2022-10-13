86f0e0f93d1e905d396a69eaa0c26c24702487f6
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-13 23:20:12 +0900

    .

---
+++ Programmers/src/level1/문자열_다루기_기본.java
@@ -0,0 +1,26 @@
+package level1;
+
+public class 문자열_다루기_기본 {
+
+	public static boolean solution(String s) {
+        boolean answer = true;
+        
+        for(int i=0; i<s.length(); i++) {
+        	char c = s.charAt(i);
+        	if((c>='A' && c<='Z')||(c>='a' && c<='z')) {
+        		answer = false;
+        		break;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		String str = "a234";
+		System.out.println(solution(str));
+		
+	}
+
+}
