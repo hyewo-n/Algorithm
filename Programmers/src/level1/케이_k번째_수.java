6725b614ed63d739f3eea76e9a5becd7338f2675
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-16 22:29:05 +0900

    .

--- Programmers/src/level1/케이_k번째_수.java
+++ Programmers/src/level1/케이_k번째_수.java
@@ -7,17 +7,14 @@ public class 케이_k번째_수 {
         int[] answer = new int[commands.length];
         
         for(int i=0; i<commands.length; i++) {
-        	int check[] = new int[commands[i][1]-commands[i][0]];
-        	for(int j=commands[i][0]; j<=commands[i][1]; i++) {
-        		int k=0;
+        	int check[] = new int[commands[i][1]-commands[i][0]+1];
+        	int k=0;
+        	for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++) {
         		check[k++] = array[j];
         	}
         	Arrays.sort(check);
-        	answer[i] = check[commands[i][3]-1];
+        	answer[i] = check[(commands[i][2])-1];
         }
-        
-        
-        
         return answer;
     }
 	
@@ -26,7 +23,9 @@ public class 케이_k번째_수 {
 		int arr[] = {1, 5, 2, 6, 3, 7, 4};
 		int com[][] = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 		
-		System.out.println(solution(arr, com));
+		for(int x : solution(arr,com))
+			System.out.println(x + " ");
+		//System.out.println(solution(arr, com));
 		
 
 	}
