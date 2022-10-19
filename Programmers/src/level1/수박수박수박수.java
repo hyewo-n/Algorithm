6e0b7abc3fbdea771bd248a658f7f675a5fd267d
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-20 04:05:53 +0900

    .

--- Programmers/src/level1/수박수박수박수.java
+++ Programmers/src/level1/수박수박수박수.java
@@ -4,6 +4,14 @@ public class 수박수박수박수 {
 
 	public static String solution(int n) {
         String answer = "";
+        
+        for(int i=1; i<=n; i++) {
+        	if(i%2==1)
+        		answer += '수';
+        	else
+        		answer += '박';
+        }
+        
         return answer;
     }
 	
