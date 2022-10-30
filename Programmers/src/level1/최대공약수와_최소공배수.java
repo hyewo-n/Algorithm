cae3435d9f0e0570d1e605461c95065ba614cb08
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-10-30 22:16:53 +0900

    .

--- Programmers/src/level1/최대공약수와_최소공배수.java
+++ Programmers/src/level1/최대공약수와_최소공배수.java
@@ -5,18 +5,18 @@ public class 최대공약수와_최소공배수 {
 	public static int[] solution(int n, int m) {
         int[] answer = new int[2];
         
-        int max = 0;
+        //int max = 0;
         
         for(int i=1; i<=n; i++) {
         	if(n%i==0 && m%i==0)
-        		max = i;
+        		answer[0] = i;
         }
-        answer[0] = max;
+        //answer[0] = max;
         
-        int min = max * (n/max) * (m/max);
+       // int min = max * (n/max) * (m/max);
         
-        answer[1] = min;
-        
+       // answer[1] = min;
+        answer[1] = answer[0] * (n/answer[0]) * (m/answer[0]);
         
         return answer;
     }
